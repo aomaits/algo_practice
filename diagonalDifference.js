5e801cc86ca784237474f96525ec65df05e08279
Abe Omaits <aomaits@gmail.com>
2023-11-16 12:41:32 -0600

    answer accepted on all test cases

--- diagonalDifference.js
+++ diagonalDifference.js
@@ -1,38 +1,38 @@
 const array1 = [ [11, 2, 4], [4, 5, 6], [10, 8, -12]]
 
 function diagonalDifference(arr) {
+    var leftToRightValue = 0;
+    var rightToLeftValue = 0;
     
     // Add the left to right diagonal
     const leftToRightDiagonal = (array) => {
-        var leftToRightValue = 0;
         
         //create the array of values to be added
         for (let i = 0; i < array.length; i++) {
             const currentSubarray = array[i];
             leftToRightValue = leftToRightValue + currentSubarray[i]
         }
-        console.log(leftToRightValue)
+        return leftToRightValue;
     }
-
+    
     const rightToLeftDiagonal = (array) => {
-        var rightToLeftValue = 0;
         let j = 0;
-
+        
         for (let i = array.length - 1; i >= 0; i--) {
             const currentSubarray = array[i];
-            // console.log(currentSubarray);
             
             rightToLeftValue = rightToLeftValue + currentSubarray[j];
             j++;
         }
-        console.log(rightToLeftValue);
-    }
+        return rightToLeftValue
+    };
 
-    // TODO - find the difference between the two diagonals! 
     leftToRightDiagonal(arr)
     rightToLeftDiagonal(arr)
 
-
+    let initialDifference = leftToRightValue - rightToLeftValue;
+    absoluteDifference = Math.abs(initialDifference)
+    console.log(absoluteDifference)
 }
 
 diagonalDifference(array1);
