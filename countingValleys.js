5cca7fe142aa6ac24401167fd3f901a0a49b16a3
Abe Omaits <aomaits@gmail.com>
2023-12-12 16:10:52 -0600

    solved this challenge!

--- countingValleys.js
+++ countingValleys.js
@@ -8,16 +8,38 @@
  */
 
 const sampleSteps1 = 8;
-const samplePath1 = ['U', 'D', 'D','D','U','D','U','U'];
+const samplePath1 = 'UDDDUDUU';
+const samplePath2 = 'DDUUDDUDUUUD'
 
 function countingValleys(steps, path) {
-    // Write your code here
-    // possible solutions- count from the beginning, if U add 1, if D subtract 1
-    // if value is negative (traversing valley) add 1 tally to valleys traversed 
-    // but don't count going further down in the same valley! 
-    // maybe create an array with the values, and if the previous array value is 0 and this one is -1 then we can add to our valley total
 
-    
+    var valleysTraversed = 0;
+    let fullHikeArray = [];
+    let currentLocation = 0; 
+
+    const splitPathArray = path.split("");
+    // console.log(splitPathArray);
+
+
+    splitPathArray.forEach(element => {
+        let lastIndexPosition = fullHikeArray.length - 1;
+
+        if (element === "U") {
+            currentLocation = currentLocation + 1 //find out where we are in relation to sea level
+            fullHikeArray.push(currentLocation);
+            // console.log(fullHikeArray)
+        }if (element === "D") {
+            currentLocation = currentLocation - 1 //find out where we are in relation to sea level
+
+            if ((fullHikeArray[lastIndexPosition]) === 0 || fullHikeArray.length < 1) {
+                valleysTraversed = valleysTraversed + 1;
+            }
+            
+            fullHikeArray.push(currentLocation);
+        }
+    });
+
+    return valleysTraversed;
 }
 
-countingValleys(sampleSteps1, samplePath1);
+countingValleys(0, samplePath2);
