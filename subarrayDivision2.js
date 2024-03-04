8d00c413e05746e4f81cef9d145719187b9164af
Abe Omaits <aomaits@gmail.com>
2024-03-04 11:49:20 -0600

    opened up a new problem to solve

---
+++ subarrayDivision2.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(s, d, m) {
+  // Write your code here
+
+}
